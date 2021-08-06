609a02d4e78fbae6e9f7debc452d7eb1cf302d0e
dkaminer1 <78710720+dkaminer1@users.noreply.github.com>
2021-08-06 16:26:29 -0400

    Update calculator_basic.py

--- calculator_basic.py
+++ calculator_basic.py
@@ -8,38 +8,20 @@ Created on Sun Jul 25 18:58:30 2021
 
 import tkinter as tk
 
-#class Application(tk.Frame):
-#    def __init__(self, master=None):
-#        super().__init__(master)
-#        self.master = master
-#        self.pack()
-#        self.create_widgets()
-#    def create_widgets(self):
-#        self.hi_there = tk.Button(self)
-#        self.hi_there["text"] = "Hello Word\n(click me)"
-#        self.hi_there["command"] = self.say_hi
-#        self.hi_there.pack(side= "top")
-#        self.quit = tk.Button(self, text = "QUIT", fg = "red", command = self.master.destroy)
-#        self.quit.pack(side = "bottom")
-#    def say_hi(self):
-#        print("hi there, everyone!")
-
-#
-
-        
-
 
 class Application(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         self.master = master
         self.grid()
-        self.create_widgets()     
-#        self.plain_text
+        self.create_widgets()   
+        
     def set_plain_text(self):
         self.plain_text = ''
+        
     def get_plain_text(self):
         return self.plain_text
+
     def update_text(self, new_text):
         self.plain_text = new_text
         
@@ -49,7 +31,6 @@ class Application(tk.Frame):
         self.display.grid(row = 6, column = 0,columnspan = 4)
         for i in range(3):
             for j in range(3):
-#                num_key= str(3 * i + j +1)
                 self.num_key = tk.Button(self, text = str(3 * i + j +1))
                 self.num_key['command'] = lambda num_value = str(3 * i + j +1) : self.add_to_string(num_value)
                 self.num_key.grid(row = i,column = j)
@@ -72,8 +53,8 @@ class Application(tk.Frame):
         self.quit.grid(row = 5,column = 2,columnspan =2)
         self.clear = tk.Button(self, text = "C", command = lambda: self.clear_key()).grid(row = 0, column = 3)
     def update(self):
-#        var = self.plain_text
-        self.display['text'] = self.plain_text
+\        self.display['text'] = self.plain_text
+
     def add_to_string(self,arg):
         new_text = self.plain_text
         new_text += arg
